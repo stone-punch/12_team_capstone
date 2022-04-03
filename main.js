c55de442d8dca1b88826057bd02260b1d96c881f
stone_punch <fpemwnsl@naver.com>
2022-04-03 12:14:55 +0900

    no message

---
+++ main.js
@@ -0,0 +1,10 @@
+var $system_1 = document.querySelector('.system_1');
+var $system_2 = document.querySelector('.system_2');
+
+$system_1.addEventListener('click',function move(){
+    location.href = "system_1.html";
+});
+
+$system_2.addEventListener('click',function(){
+    location.href = "system_2.html";
+});
